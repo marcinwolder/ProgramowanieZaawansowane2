c0cdb5477850d5c5daf1c30695430606e230c4f5
marcinwolder marcinwolder <marcinwolder@net.kis.agh.edu.pl@r221-c12.net.kis.agh.edu.pl>
2025-06-05 12:45:04 +0200

    ticket added

--- lab-09/Airly/Models/User.cs
+++ lab-09/Airly/Models/User.cs
@@ -8,4 +8,5 @@ public class User
     public int Id { get; set; }
     required public string Email { get; set; }
     required public string Password { get; set; }
+    public ICollection<Ticket> ?Tickets { get; set; }
 }
